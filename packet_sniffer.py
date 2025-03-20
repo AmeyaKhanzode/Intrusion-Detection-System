af7e78cfc7fbdb211f835513b602e7c45340d3c1
AmeyaKhanzode <ameyakhanzode2022@gmail.com>
2025-03-21 00:26:00 +0530

    fixed packet sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,7 +4,7 @@ import struct
 
 try:
     sock = socket(AF_PACKET, SOCK_RAW, ntohs(0x0003))  # ipv4, tcp
-    sock.bind(("wlan0", 0))
+    sock.bind(("any", 0))
     print("Socket created.")
 except error as err:
     print("Couldn't create socket:", err)
@@ -13,12 +13,11 @@ print("Socket is listening...\n")
 
 while True:
     packet, addr = sock.recvfrom(65565)  # for packets upto size 65565
-    print("Got tcp packet from:", addr)
-    print("Raw Packet:", packet)
-    print()
+    # print("Got tcp packet from:", addr)
+    # print()
 
     # IP Header Unpacking
-    ip_header = packet[:20]
+    ip_header = packet[14:34]
     ip_header_unpacked = struct.unpack("!BBHHHBBH4s4s", ip_header)
 
     '''
@@ -39,32 +38,24 @@ while True:
     packet_size = ip_header_unpacked[2]
     ttl = ip_header_unpacked[5]
 
-    if protocol == 6:
-        print("Protocol: TCP")
-    elif protocol == 17:
-        print("Protocol: UDP")
-
-    print("TTL: ", ttl)
-    print("Packet size: ", packet_size)
-
-    print("Source IP: ", src_ip)
-    print("Destination IP: ", dest_ip)
-
     # TCP Header Unpacking
-    tcp_header = packet[ip_header_length: ip_header_length + 20]
+    tcp_header = packet[ip_header_length + 14: ip_header_length + 34]
     tcp_header_unpacked = struct.unpack("!HHIIHHHH", tcp_header)
 
     src_port = tcp_header_unpacked[0]
     dest_port = tcp_header_unpacked[1]
     tcp_flags = tcp_header_unpacked[5]
     seq_num = tcp_header_unpacked[2]
-    tcp_header_length = tcp_header_unpacked[4]
+    tcp_header_length = (tcp_header_unpacked[4] >> 12) * 4
+    print("protocol: ", protocol)
+    if protocol == 6:
+        print("tcp flags: ", bin(tcp_flags))
+        print("Protocol: ", protocol)
+        print("src port: ", src_port)
+        print("dest port: ", dest_port)
+        print("TTL: ", ttl)
+        print("Packet size: ", packet_size)
+        print("Source IP: ", src_ip)
+        print("Destination IP: ", dest_ip)
 
-    print("tcp flags: ", bin(tcp_flags))
-    print("Protocol: ", protocol)
-    print("src port: ", src_port)
-    print("dest port: ", dest_port)
-
-    payload = packet[tcp_header_length + 20:]
-    print("Payload: ", payload)
     time.sleep(1)
