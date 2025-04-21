24e5d08a895484140a046b9b22c416244a61626c
Ameya Khanzode <ameyakhanzode2022@gmail.com>
2025-04-22 00:57:35 +0530

    fixed tmux to start all files

--- start.sh
+++ start.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-# to start first window to show tcp packets
-tmux new-session -d -s ids_session
-sleep 0.1
-tmux send-keys -t ids_session "sudo python3 packet_sniffer.py" C-m
-tmux split-window -h -t ids_session
-sleep 0.1
-tmux send-keys -t ids_session:0.1 "nc -lvnp 9090" C-m
-tmux split-window -v -t ids_session
-sleep 0.1
-tmux send-keys -t ids_session:0.2 "echo 'sup lalith, isnt this kinda cool' | nc 127.0.0.1 9090" C-m
 
-# to start second window to detect brute force
-tmux new-window -t ids_session
-sleep 0.1
-tmux send-keys -t ids_session:1.0 "sudo python3 packet_sniffer.py" C-m
-tmux split-window -h -t ids_session:1
-sleep 0.1
-tmux send-keys -t ids_session:1.1 "cd ~/CN_Project/Intrusion-Detection-System/detection/" C-m
-sleep 0.1
-tmux send-keys -t ids_session:1.1 "sudo python3 brute_force_detector.py" C-m
-tmux split-window -v -t ids_session:1
+# Start a new tmux session in detached mode
+tmux new-session -d -s ids_session -n main
+
+# Pane 0: packet_sniffer.py
+tmux send-keys -t ids_session:0.0 "sudo python3 packet_sniffer.py" C-m
 sleep 0.2
-tmux send-keys -t ids_session:1.2 "sudo hping3 -S -i u1000 -p 22 127.0.0.1" C-m
 
-tmux select-window -t ids_session:0
+# Split Pane 0 horizontally -> Pane 1 (right of Pane 0)
+tmux split-window -h -t ids_session:0.0
+sleep 0.2
+# Pane 1: arp_spoofing.py
+tmux send-keys -t ids_session:0.1 "cd ~/Intrusion-Detection-System/detection && sudo python3 arp_spoofing.py" C-m
+sleep 0.2
+
+# Split Pane 0 vertically -> Pane 2 (below Pane 0)
+tmux split-window -v -t ids_session:0.0
+sleep 0.2
+tmux send-keys "cd ~/Intrusion-Detection-System/detection/" C-m
+tmux send-keys "python3 brute_force_detector.py" C-m
+
+sleep 0.2
+
+tmux split-window -v -t ids_session:0.2
+sleep 0.2
+tmux send-keys "cd ~/Intrusion-Detection-System/detection/" C-m
+tmux send-keys "python3 port_scan_detector.py" C-m
+
+# Attach the session
 tmux attach -t ids_session
